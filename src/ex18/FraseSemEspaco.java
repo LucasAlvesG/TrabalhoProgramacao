cb0a1cae348d5c79908e0b8c63af1d03b1650b7b
marivaz0 <mariellerodrigues62@gmail.com>
2023-05-26 14:02:28 -0300

    ex18 feito

--- src/ex18/FraseSemEspaco.java
+++ src/ex18/FraseSemEspaco.java
@@ -5,15 +5,11 @@ import javax.swing.*;
 public class FraseSemEspaco {
     String frase = "";
 
-    /*const qtde = (frase.match(/ /g) || []).length
-    console.log("Quantidade de espaços na frase: " + qtde)*/
-
     public void verificarFrase(){
         frase = JOptionPane.showInputDialog("Infome uma frase de até 50 caracteres!");
         String f_sem = frase.replaceAll(" ","");
 
-        int qtde = 0;
-        qtde = (frase.length() - f_sem.length());
+        int qtde = (frase.length() - f_sem.length());
 
         JOptionPane.showMessageDialog(null, "Frase informada: " +frase + "\nFrase sem espaços: " + f_sem
                 + "\nQuantidade de espaços: " + qtde);
